9985a5361751b773b33293cffb36dd8843979fa2
twodragon <100879611+twodragon@users.noreply.github.com>
2023-09-11 22:12:18 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -26,16 +26,19 @@ var (
 func main() {
 	// Bağlanılacak sunucunun IP adresi ve portu karakter id username ve parolası
 	serverAddr := "188.132.128.230:4515"
-	cid = 1044
-	username = "id"
-	password := "şifre"
+	cid = 1086 //1054              //1046nish
+	username = "pazar4"//"kinkordun"
+	password := "annen"//"akordiyonu"
 
-	Location = ConvertPointToLocation("(313.0,161.0)")
-	Target = ConvertPointToLocation("(313.0,163.0)")
+	Location = ConvertPointToLocation("(245.0,125.0)")
+	Target = ConvertPointToLocation("(245.0,127.0)")
+	
+	
 	// Sunucuya bağlan
 	conn, err := net.Dial("tcp", serverAddr)
 	if err != nil {
 		fmt.Println("Sunucuya bağlanırken hata oluştu:", err)
+		
 		return
 	}
 
@@ -64,14 +67,14 @@ func main() {
 
 }
 
-func sendstring(conn net.Conn, packet string) {
+func (s *Socket) sendstring(packet string) {
 	data, err := hex.DecodeString(packet)
 	if err != nil {
 		fmt.Println("Paket dönüştürülürken hata oluştu:", err)
 		return
 	}
 
-	_, err = conn.Write(data)
+	_, err = s.Conn.Write(data)
 	if err != nil {
 		fmt.Println("Paket gönderilirken hata oluştu:", err)
 		return
@@ -114,6 +117,7 @@ func (s *Socket) handleResponse(conn net.Conn) {
 	response, err := s.recognizePacket(buffer[:n])
 	if err != nil {
 		log.Println("recognize packet error:", err)
+		
 	}
 	if len(response) < 0 {
 		return
@@ -124,7 +128,7 @@ func (s *Socket) handleResponse(conn net.Conn) {
 
 func (s *Socket) recognizePacket(data []byte) ([]byte, error) { //test read pkg example with chat types
 	pkgType := utils.BytesToInt(data[4:6], false)
-	if pkgType == 19714 || pkgType == 8706 || pkgType == 8705 || pkgType == 22809 {
+	if pkgType == 19714 || pkgType == 8706 || pkgType == 8705 || pkgType == 22809 /*|| pkgType ==  33544*/ {
 		return nil, nil
 	} else {
 		//log.Printf("%d", pkgType)
@@ -153,6 +157,7 @@ func (s *Socket) recognizePacket(data []byte) ([]byte, error) { //test read pkg 
 	case 8705: //yanına gelen karakterlerin movementi
 	case 8706: //yanına gelen karakterlerin movementi
 	case 29185:
+	//log.Printf("Hoşgeldin")
 		movement := utils.Packet{0xaa, 0x55, 0x21, 0x00, 0x22, 0x01,
 			0x00, 0x9c, 0x81, 0x40,
 			0x00, 0x9c, 0x81, 0x40, 0x5a, 0x00, 0x00, 0x00, 0xa0, 0x40, 0x00, 0x55, 0xaa}
@@ -167,12 +172,78 @@ func (s *Socket) recognizePacket(data []byte) ([]byte, error) { //test read pkg 
 		s.sendbyte(movement)
 		ping := utils.Packet{0xaa, 0x55, 0x01, 0x00, 0x18, 0x55, 0xaa}
 		s.sendbyte(ping)
+		//pazarhazr := "aa555a0055010e6e65206f6c757220616c20616269060b0000001caf7d1a000000000c0001001caf7d1a000000000d0002001caf7d1a000000000e0003001caf7d1a000000000f0004001caf7d1a00000000100005001caf7d1a0000000055aa"
+		//pazarhazr := "aa5562005501165175616c69747920676f6f6473206f6e2073616c6521060b0000001caf7d1a000000000c0001001caf7d1a000000000d0002001caf7d1a000000000e0003001caf7d1a000000000f0004001caf7d1a00000000100005001caf7d1a0000000055aa"
+		//s.sendstring(pazarhazr)
+		log.Printf("Hoşgeldin pazar botu aktif")
+		
+		//pazar func
+		pazarbot := utils.Packet{0xaa, 0x55, 0xff, 0x00, 0x55, 0x01,
+		0x06, 0x0b, 0x00, 0x00, 0x00,
+		0x0c, 0x00, 0x01, 0x00,
+		0x0d, 0x00, 0x02, 0x00,
+		0x0e, 0x00, 0x03, 0x00,
+		0x0f, 0x00, 0x04, 0x00,
+		0x10, 0x00, 0x05, 0x00,
+		0x55, 0xaa}
+		pindex := 6
+		
+		pazarismi := "Qualite goods on!amk"
+		var fiyat1 uint64 = 34444144444
+		var fiyat2 uint64 = 34444424444
+		var fiyat3 uint64 = 34444484444
+		var fiyat4 uint64 = 34444444444
+		var fiyat5 uint64 = 34444644444
+		var fiyat6 uint64 = 34443444444
+		
+		
 
+		pazarbot.Insert([]byte{byte(len(pazarismi))}, pindex) //isim uzunluğu bastır
+		pindex += 1
+		pazarbot.Insert([]byte(pazarismi), pindex) //isim bastır
+		pindex += len(pazarismi) + 5
+		pazarbot.Insert(utils.IntToBytes(uint64(fiyat1), 8, true), pindex)
+		pindex += 12
+		pazarbot.Insert(utils.IntToBytes(uint64(fiyat2), 8, true), pindex)
+		pindex += 12
+		pazarbot.Insert(utils.IntToBytes(uint64(fiyat3), 8, true), pindex)
+		pindex += 12
+		pazarbot.Insert(utils.IntToBytes(uint64(fiyat4), 8, true), pindex)
+		pindex += 12
+		pazarbot.Insert(utils.IntToBytes(uint64(fiyat5), 8, true), pindex)
+		pindex += 12
+		pazarbot.Insert(utils.IntToBytes(uint64(fiyat6), 8, true), pindex)
+		lengthpaz := len(pazarismi) + 76
+		pazarbot.SetLength(int16(lengthpaz)) 
+		s.sendbyte(pazarbot)
+		//pazarfunc end
+		
 		/*coordinate := &utils.Location{X: utils.BytesToFloat(data[6:10], true), Y: utils.BytesToFloat(data[10:14], true)}
 		target := &utils.Location{X: utils.BytesToFloat(data[18:22], true), Y: utils.BytesToFloat(data[22:26], true)}*/
 		/*cordgo := "aa552100220100809c43000a2143009c814000809c4300002343009c81405a000000a0400055aa"
-		sendstring(conn, cordgo)*/
+		sendstring(cordgo)*/
 
+		silahcikart := utils.Packet{0xaa, 0x55, 0x02, 0x00, 0x43, 0x01, 0x55, 0xaa} //silahı ele al
+		s.sendbyte(silahcikart)
+		
+			case 33544:
+	log.Println("Hoşgeldin")
+		movement := utils.Packet{0xaa, 0x55, 0x21, 0x00, 0x22, 0x01,
+			0x00, 0x9c, 0x81, 0x40,
+			0x00, 0x9c, 0x81, 0x40, 0x5a, 0x00, 0x00, 0x00, 0xa0, 0x40, 0x00, 0x55, 0xaa}
+
+		coordinate := &utils.Location{X: Location.X, Y: Location.Y}
+		target := &utils.Location{X: Target.X, Y: Target.Y}
+
+		movement.Insert(utils.FloatToBytes(coordinate.X, 4, true), 6)  // coordinate-x
+		movement.Insert(utils.FloatToBytes(coordinate.Y, 4, true), 10) // coordinate-y
+		movement.Insert(utils.FloatToBytes(target.X, 4, true), 18)     // target-x
+		movement.Insert(utils.FloatToBytes(target.Y, 4, true), 22)     // target-y
+		s.sendbyte(movement)
+		ping := utils.Packet{0xaa, 0x55, 0x01, 0x00, 0x18, 0x55, 0xaa}
+		s.sendbyte(ping)
+		pazarhazr := "aa555a0055010e6e65206f6c757220616c20616269060b0000001caf7d1a000000000c0001001caf7d1a000000000d0002001caf7d1a000000000e0003001caf7d1a000000000f0004001caf7d1a00000000100005001caf7d1a0000000055aa"
+		s.sendstring(pazarhazr)
 		silahcikart := utils.Packet{0xaa, 0x55, 0x02, 0x00, 0x43, 0x01, 0x55, 0xaa} //silahı ele al
 		s.sendbyte(silahcikart)
 	case 28929: // normal chat
@@ -195,6 +266,13 @@ func (s *Socket) recognizePacket(data []byte) ([]byte, error) { //test read pkg 
 		messageLen := utils.BytesToInt([]byte{data[9+charlen]}, true)
 		message := string(data[10+charlen : 11+charlen+messageLen])
 		log.Printf(fmt.Sprintf("# %s: %s \n", charname, message))
+		
+			/*case 28936: // test
+		charlen := utils.BytesToInt([]byte{data[8]}, true)
+		charname := string(data[8 : 9+charlen])
+		messageLen := utils.BytesToInt([]byte{data[9+charlen]}, true)
+		message := string(data[10+charlen : 11+charlen+messageLen])
+		log.Printf(fmt.Sprintf(" %s: %s \n", charname, message))*/
 
 	case 28946: // valorus chat
 		charlen := utils.BytesToInt([]byte{data[6]}, true)
